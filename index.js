30ef149c7040128b4523208474d24c9b8c762358
GauravN1kam <79043460+GauravN1kam@users.noreply.github.com>
2021-10-31 20:31:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+for (
+    var index = 0; index < document.querySelectorAll(".drum").length; index++
+) {
+    document
+        .querySelectorAll("button")[index].addEventListener("click", function () {
+            var buttonHTML = this.innerHTML;
+            check(buttonHTML);
+        });
+}
+document.addEventListener("keypress",
+    function () 
+    {
+        check(event.key);
+    }
+)
+
+function check(buttonHTML)
+{
+    switch (buttonHTML) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
